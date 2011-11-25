f41d4bb9735bdcfd24621c09c8262078f22c9fb8
kn810609 <kn810609@fa7d8220-63e9-7aaf-beab-20f0db7c1a03>
2011-11-25 16:21:05 +0000

    Scene      modified IceCreater completed

--- icebreak/Assets/demo/GlassWater.js
+++
@@ -1,7 +0,0 @@
-var glass : Texture2D;
-
-function OnGUI () {
-	var padPos:Vector2 = Vector2(Screen.width-200, Screen.height*0.7);
-	GUI.DrawTexture(Rect(padPos.x , padPos.y ,200  ,250), 
-		  								glass, ScaleMode.StretchToFill, true, 0);
-}
--- icebreak/Assets/demo/exit.js
+++
@@ -1,6 +0,0 @@
-
-function OnGUI () {
-	if(GUI.Button(Rect(0,0,100,100),"Exit" )){
-												Application.Quit();
-	}
-}
